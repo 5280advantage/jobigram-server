810bbc6f51b9597e5a4d1fe014f7900049c0714a
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-08-01 20:16:45 -0300

    Push Fix

--- cloud/class/GalleryActivity.js
+++ cloud/class/GalleryActivity.js
@@ -1,13 +1,13 @@
 'use strict';
-const _           = require('lodash');
-const User        = require('./../class/User');
+const _ = require('lodash');
+const User = require('./../class/User');
 const ParseObject = Parse.Object.extend('GalleryActivity');
-const UserFollow  = Parse.Object.extend('UserFollow');
+const UserFollow = Parse.Object.extend('UserFollow');
 
 module.exports = {
     afterSave: afterSave,
-    create   : create,
-    feed     : feed,
+    create: create,
+    feed: feed,
 };
 
 function afterSave(req, res) {
@@ -15,100 +15,65 @@ function afterSave(req, res) {
     //    return
     //}
 
-    // Send Notification toUser
-    const toUser = req.object.get('toUser');
-    if (!toUser) {
+    if (!req.object.get('toUser')) {
         throw 'Undefined toUser. Skipping push for Activity ' + req.object.get('action') + ' : ' + req.object.id;
         return;
     }
 
-    let data = alertPayload(req);
-    console.log('Push', toUser.username, data);
-
-    Parse.Push.send(
-        {
-            channels: [toUser.username],
-            data   : data
-        },{ useMasterKey: true })
-         .then(function() {
-             console.log('push sent. args received: ' + JSON.stringify(arguments) + '\n');
-             res.success({status: 'push sent', ts: Date.now()});
-         }, function(error) {
-             console.log('push failed. ' + JSON.stringify(error) + '\n');
-             res.error(error);
-         });
+    console.log('afterSave', req);
 
 
+    Parse.Promise.when([
+        new Parse.Query('User').equalTo('objectId', req.object.get('fromUser').id).first({
+            useMasterKey: true
+        }),
+        new Parse.Query('User').equalTo('objectId', req.object.get('toUser').id).first({
+            useMasterKey: true
+        })
+    ]).then(result => {
+        const fromUser = result[0];
+        const toUser = result[1];
+        const action = req.object.get('action');
+        let channel = toUser.attributes.username;
+
+        Parse.Push.send({
+                channels: [channel],
+                data: {
+                    alert: alertMessage(action, result[0].attributes.name), // Set our alert message.
+                    badge: 'Increment', // Increment the target device's badge count.
+                    // The following keys help Anypic load the correct photo in response to this push notification.
+                    p: 'a', // Payload Type: Activity
+                    t: 'c', // Activity Type: Comment
+                    fu: fromUser.id, // From User
+                    pid: toUser.id // Photo Id
+                }
+            }, {
+                useMasterKey: true
+            })
+            .then(function() {
+                console.log('push sent. args received: ' + JSON.stringify(arguments) + '\n');
+                res.success({
+                    status: 'push sent',
+                    ts: Date.now()
+                });
+            }, function(error) {
+                console.log('push failed. ' + JSON.stringify(error) + '\n');
+                res.error(error);
+            });
+    });
 }
 
-function alertPayload(req) {
+function alertMessage(action, userName) {
 
-    switch (req.object.get('action')) {
-        case 'comment':
-            return {
-                alert: alertMessage(req), // Set our alert message.
-                badge: 'Increment', // Increment the target device's badge count.
-                // The following keys help Anypic load the correct photo in response to this push notification.
-                p    : 'a', // Payload Type: Activity
-                t    : 'c', // Activity Type: Comment
-                fu   : req.object.get('fromUser').id, // From User
-                pid  : req.object.id // Photo Id
-            };
-            break;
+    let message = '';
+    console.log('alertMessage', action)
 
-        case 'like':
-            return {
-                alert: alertMessage(req), // Set our alert message.
-                // The following keys help Anypic load the correct photo in response to this push notification.
-                p    : 'a', // Payload Type: Activity
-                t    : 'l', // Activity Type: Like
-                fu   : req.object.get('fromUser').id, // From User
-                pid  : req.object.id // Photo Id
-            };
-            break;
-
-        case 'followUser':
-            return {
-                alert: alertMessage(req), // Set our alert message.
-                // The following keys help Anypic load the correct photo in response to this push notification.
-                p    : 'a', // Payload Type: Activity
-                t    : 'f', // Activity Type: Follow
-                fu   : req.object.get('fromUser').id // From User
-            };
-            break
-    }
-
-}
-
-function alertMessage(req) {
-    var message = '';
-
-    switch (req.object.get('action')) {
-        case 'comment':
-            if (req.user.get('name')) {
-                message = req.user.get('name') + ': ' + req.object.get('content').trim();
-            } else {
-                message = req.user.get('name') + ' commented on your photo.';
-            }
-            return message;
-            break;
-        case 'like':
-            if (req.user.get('name')) {
-                message = req.user.get('name') + ' likes your photo.';
-            } else {
-                message = 'Someone likes your photo.';
-            }
-            return message;
-            break;
-
-        case 'followUser':
-            if (req.user.get('name')) {
-                message = req.user.get('name') + ' is now following you.';
-            } else {
-                message = 'You have a new follower.';
-            }
-            return message;
-            break;
+    if (action === 'comment') {
+        message = userName + ' commented on your photo.';
+    } else if (action === 'like') {
+        message = userName + ' likes your photo.';
+    } else if (action === 'followUser') {
+        message = userName + ' is now following you.';
     }
 
     // Trim our message to 140 characters.
@@ -116,8 +81,10 @@ function alertMessage(req) {
         message = message.substring(0, 140);
     }
 
+    console.log('message', action, message)
     return message;
 }
+
 function create(obj, acl) {
 
     let newActivity = new ParseObject()
@@ -129,26 +96,17 @@ function create(obj, acl) {
         newActivity.set('toUser', obj.toUser);
     }
 
-
     if (acl) {
         newActivity.setACL(acl);
     }
 
-    if (obj.gallery) {
-        newActivity.set('gallery', obj.gallery);
-        new Parse.Query('Gallery').include('user').equalTo('objectId', obj.gallery.id).first().then(gallery=> {
-            return newActivity.set('toUser', gallery.get('user')).save(null, {useMasterKey: true});
-        });
-
-    } else {
-        return newActivity.save(null, {useMasterKey: true});
-    }
-
-
+    return newActivity.save(null, {
+        useMasterKey: true
+    });
 }
 
 function feed(req, res, next) {
-    const _page  = req.params.page || 1;
+    const _page = req.params.page || 1;
     const _limit = req.params.limit || 10;
 
     console.log('Start feed', req.params);
@@ -159,26 +117,28 @@ function feed(req, res, next) {
         .include('gallery')
         .equalTo('toUser', req.user)
         .skip((_page * _limit) - _limit)
-        .find({useMasterKey: true})
-        .then(data=> {
+        .find({
+            useMasterKey: true
+        })
+        .then(data => {
             let _result = [];
 
             if (!data.length) {
                 res.success(_result);
             }
 
-            let cb = _.after(data.length, ()=> {
+            let cb = _.after(data.length, () => {
                 res.success(_result);
             });
 
-            _.each(data, item=> {
+            _.each(data, item => {
 
                 let userGet = item.get('fromUser');
-                new Parse.Query('UserData').equalTo('user', userGet).first().then(user=> {
+                new Parse.Query('UserData').equalTo('user', userGet).first().then(user => {
 
                     let obj = {
-                        item     : item,
-                        action   : item.get('action'),
+                        item: item,
+                        action: item.get('action'),
                         createdAt: item.get('createdAt'),
                     };
 
@@ -189,15 +149,15 @@ function feed(req, res, next) {
                             .equalTo('from', req.user)
                             .equalTo('to', user)
                             .count()
-                            .then(isFollow=> {
+                            .then(isFollow => {
                                 console.log(isFollow);
                                 obj.user = {
-                                    obj     : user,
-                                    id      : user.id,
-                                    name    : user.get('name'),
+                                    obj: user,
+                                    id: user.id,
+                                    name: user.get('name'),
                                     username: user.get('username'),
-                                    status  : user.get('status'),
-                                    photo   : user.get('photo'),
+                                    status: user.get('status'),
+                                    photo: user.get('photo'),
                                     isFollow: isFollow > 0 ? true : false
                                 };
                                 _result.push(obj);
@@ -211,10 +171,10 @@ function feed(req, res, next) {
                         cb();
                     }
 
-                }, err=>console.log);
+                }, err => console.log);
 
             });
 
 
-        }, error=> res.error(error.message));
+        }, error => res.error(error.message));
 }
